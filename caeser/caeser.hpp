0d25e9ca93b7e428b0952aa36478cd2b79a76c08
Bhavya Kapoor <bhavyarocks19@gmail.com>
2019-10-11 17:45:57 +0530

    made

---
+++ caeser/caeser.hpp
@@ -0,0 +1,60 @@
+#ifndef CAESER_HPP
+#define CAESER_HPP
+
+namespace maurya
+{
+    #include <string>
+    #include <ctype.h>
+
+    using namespace std;
+
+    string caesercipher(string &str, int shift)
+    {
+        string enc;
+        char x;
+        int temp = shift;
+        for (int i = 0; i < str.size(); i++)
+        {
+            shift = temp;
+            x = str[i];
+            if(isalnum(x))
+            {
+                if(isalpha(x))
+                {
+                    shift %= 26;
+                    if(shift < 0)
+                        shift += 26;
+                    if (isupper(x))
+                    {
+                        x -= 65;
+                        x += shift;
+                        x %= 26;
+                        x += 65;
+                    }
+                    else
+                    {                 
+                        x -= 97;
+                        x += shift;
+                        x %= 26;
+                        x += 97;
+                    }
+                }
+                else
+                {                
+                    shift %= 10;
+                    if(shift < 0)
+                        shift += 10;
+                    x -= 48;
+                    x += shift;
+                    x %= 10;
+                    x += 48;
+                }
+            }
+            enc.push_back(x);
+        }
+        return enc;
+    }
+
+} // namespace maurya
+
+#endif
